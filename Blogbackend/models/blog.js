f97ca7553eb5ae7b98de0c5ea518ed6d4fcf22d1
Sami <samialeksi.tuomela@gmail.com>
2024-01-28 17:11:40 +0200

    added 13.17, 13.18

--- Blogbackend/models/blog.js
+++ Blogbackend/models/blog.js
@@ -8,7 +8,15 @@ Blog.init(
     author: { type: DataTypes.TEXT },
     url: { type: DataTypes.TEXT, allowNull: false },
     title: { type: DataTypes.TEXT, allowNull: false },
-    likes: { type: DataTypes.INTEGER, defaultValue: 0 }
+    likes: { type: DataTypes.INTEGER, defaultValue: 0 },
+    yearWritten: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1991,
+        max: new Date().getFullYear()
+      }
+    }
   },
   { sequelize, underscored: true, timestamps: false, modelName: "blog" }
 )
